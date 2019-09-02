e060cc98b2aed0090235384cabf51497e0e85874
Mallory Minchau <mallory.m.minchau@gmail.com>
2019-09-02 22:20:11 +0000

    get random string function added to express server file as well as urls new file added

--- express_server.js
+++ express_server.js
@@ -1,8 +1,18 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 8080; // default port 8080
-
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+function generateRandomString() {
+  let stringId = '';
+  let alphaNumeric = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0987654321';
+  for (let i = 0; i < 6; i++) {
+    stringId += alphaNumeric.charAt(Math.floor(Math.random() * alphaNumeric.length));
+  }
+  return stringId;
+}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -32,7 +42,17 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 })
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
   res.render("urls_show", templateVars);
 })
+
