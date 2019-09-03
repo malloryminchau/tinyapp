b085719cd19a7156345b61dc0dcea9b8e30cea33
Mallory Minchau <mallory.m.minchau@gmail.com>
2019-09-03 18:40:12 +0000

    git added delete button to urls homepage

--- express_server.js
+++ express_server.js
@@ -49,6 +49,7 @@ app.post("/urls", (req, res) => {
   res.redirect(`./urls/${newURL}`);         // Respond with 'Ok' (we will replace this)
 });
 
+
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
@@ -57,7 +58,10 @@ app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 })
-
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls")
+})
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   if (longURL) {
